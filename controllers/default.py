795180b4d0606f137908a27c2e4ae7bbeafd299f
ecepernich <ecepernich@gmail.com>
2016-12-29 00:24:51 -0800

    finishing for the night

--- controllers/default.py
+++ controllers/default.py
@@ -2,22 +2,19 @@
 ## controllers
 #########################################################################
 
-def index():
-    items=db(db.item.sold == False).select()
-    return locals()
-
-
 def user():
     return dict(form=auth())
-
 
 @cache.action()
 def download():
     return response.download(request, db)
 
-
 def call():
     return service()
+
+def index():
+    items=db(db.item.sold == False).select()
+    return locals()
 
 def listing():
     post = db.item(request.args(0, cast=int))
@@ -30,7 +27,7 @@ def orderform():
     form = SQLFORM(db.orderform).process()
     if form.accepted:
         post.update_record(sold=True)
-        redirect(URL('index'))
+        redirect(URL('payment', args=post.id))
     return locals()
 
 def pending():
